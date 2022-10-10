cf1a4f5d33869ae96fa8ed23afb01d81c75bea6c
Dũng Nguyễn <dungnm243@gmail.com>
2022-10-11 05:07:04 +0700

    11/10/2022
    Tạo cấu trúc cơ bản cho chương trình

---
+++ src/main/java/group5/libraryManagementWebApp/LibraryManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package group5.libraryManagementWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group5/libraryManagementWebApp/LibraryManagementWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.libraryManagementWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
